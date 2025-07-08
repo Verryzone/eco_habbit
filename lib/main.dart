39a5fd2e454b4a50f0ca0b1cddc54110ceb384dd
Verryzone <umu369852147@gmail.com>
2025-07-08 13:48:09 +0700

    instalasi supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:eco_habbit/config/env.dart';
 import 'package:eco_habbit/pages/splashScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+  await Supabase.initialize(
+    url: AppEnv.supabaseUrl,
+    anonKey: AppEnv.supabaseAnonKey,
+  );
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
